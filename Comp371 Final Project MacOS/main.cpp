4348d6a23d84691de5f9a2a4c239be62f472d824
Bruno Robert <bruno.jeanluke@gmail.com>
2018-11-06 16:27:40 -0500

    Initial Commit

---
+++ Comp371 Final Project MacOS/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Comp371 Final Project MacOS
+//
+//  Created by Bruno Robert on 06/11/2018.
+//  Copyright © 2018 Bruno Robert. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
