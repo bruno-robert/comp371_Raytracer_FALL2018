a7db8b55463f3330189d0ea864cd6f62cd218bce
Bruno Robert <bruno.jeanluke@gmail.com>
2018-11-07 10:11:08 -0500

    Minor bug fixes and Improvements Added Deconstructor Added Verbose Option for debugging Added Camera Object loading Started triangle Object  loading Added getLineValue helper functions Added Triangle Object

--- Comp371 Final Project MacOS/SceneLoader.cpp
+++ Comp371 Final Project MacOS/SceneLoader.cpp
@@ -10,13 +10,14 @@ SceneObj::SceneObj(const char * path) {
 	numberOfObjects = 0;
 }
 
+SceneObj::~SceneObj() {
+    //TODO: delete objectArray properly
+}
+
 bool SceneObj::loadScene() {
-	
-	//loop through file and instanciate each object
 	std::ifstream ifs;
 	ifs.open(path, std::fstream::in);
-	if (!ifs.is_open())
-	{
+	if (!ifs.is_open()) {
 		std::cout << "Error - cannot open the file." << std::endl;
 		return 0;
 	}
@@ -31,21 +32,194 @@ bool SceneObj::loadScene() {
 			numberOfObjects = std::stoi(line);
 		}
 		catch (std::exception const &e) {
+            std::cout << "the number of objects wasn't translated to an integer" << std::endl;
 		}
-		
 	}
-
-    std::cout << "The scene has " << numberOfObjects << " objects" << std::endl;
-
+    //MARK: verbose
+    if (VERBOSE) {
+        std::cout << "The scene has " << numberOfObjects << " objects" << std::endl;
+    }
+    
+    //loop through file and instanciate each object
 	while (ifs.good()) {//check if ther is a line
 		ifs.getline(line, LINE_SIZE);//retrieve current line
-		//std::cout << line << std::endl;
-	}
+        //std::cout << line << std::endl;//MARK: removable
+        std::string currentLine = line;
+        if(currentLine == "camera" || currentLine == "camera\n" || currentLine == "camera\r\n" || currentLine == "camera\r") {
+            glm::vec3 pos = glm::vec3(0.0f);//position
+            int fov = 0;//field of view
+            int fl = 0;//focal length
+            float ar = 0;//aspectratio
+            bool err = false;
+            if(ifs.good()) {//get position
+                ifs.getline(line, LINE_SIZE);
+                currentLine = line;
+                if(!getLineValue(line, &pos, "pos:"))//get the value
+                    err = true;//if something went wrong set err to true
+            } else {
+                err = true;
+            }
+            if(ifs.good()) {//get fov
+                ifs.getline(line, LINE_SIZE);
+                currentLine = line;
+                if(!getLineValue(line, &fov, "fov:"))
+                    err = true;
+            } else {
+                err = true;
+            }
+            if(ifs.good()) {//get fl
+                ifs.getline(line, LINE_SIZE);
+                currentLine = line;
+                if(!getLineValue(line, &fl, "f:"))
+                    err = true;
+            } else {
+                err = true;
+            }
+            if(ifs.good()) {//get ar
+                ifs.getline(line, LINE_SIZE);
+                currentLine = line;
+                if(!getLineValue(line, &ar, "a:"))
+                    err = true;
+            } else {
+                err = true;
+            }
+            if(err) {
+                std::cout << "missing data in camera (something went wrong)" << std::endl;
+            } else {
+                Camera Camera(pos, fov, fl, ar);
+                cameraArray.push_back(Camera);
+            }
+            //MARK: verbose
+            if (VERBOSE) {
+                std::cout << "Camera " << cameraArray.size() << " set!" << std::endl;//FIXME: this doesn't work if it fails, only when it doesn't
+            }
+        }
+        else if(currentLine == "triangle" || currentLine == "triangle\n" || currentLine == "triangle\r\n" || currentLine == "triangle\r") {
+            glm::vec3 v1;
+            glm::vec3 v2;
+            glm::vec3 v3;
+            glm::vec3 ambientColor;
+            glm::vec3 diffuseColor;
+            glm::vec3 specularColor;
+            float shininess;
+            bool err = false;
+            
+            if(ifs.good()) {//get position
+                ifs.getline(line, LINE_SIZE);
+                currentLine = line;
+                if(!getLineValue(line, &v1, "v1:"))//get the value
+                    err = true;//if something went wrong set err to true
+            } else {
+                err = true;
+            }
+            if(ifs.good()) {//get position
+                ifs.getline(line, LINE_SIZE);
+                currentLine = line;
+                if(!getLineValue(line, &v2, "v2"))//get the value
+                    err = true;//if something went wrong set err to true
+            } else {
+                err = true;
+            }
+            if(ifs.good()) {//get position
+                ifs.getline(line, LINE_SIZE);
+                currentLine = line;
+                if(!getLineValue(line, &v3, "v3:"))//get the value
+                    err = true;//if something went wrong set err to true
+            } else {
+                err = true;
+            }
+            if(ifs.good()) {//get position
+                ifs.getline(line, LINE_SIZE);
+                currentLine = line;
+                if(!getLineValue(line, &ambientColor, "amb:"))//get the value
+                    err = true;//if something went wrong set err to true
+            } else {
+                err = true;
+            }
+            if(ifs.good()) {//get position
+                ifs.getline(line, LINE_SIZE);
+                currentLine = line;
+                if(!getLineValue(line, &diffuseColor, "dif:"))//get the value
+                    err = true;//if something went wrong set err to true
+            } else {
+                err = true;
+            }
+            if(ifs.good()) {//get position
+                ifs.getline(line, LINE_SIZE);
+                currentLine = line;
+                if(!getLineValue(line, &specularColor, "spe:"))//get the value
+                    err = true;//if something went wrong set err to true
+            } else {
+                err = true;
+            }
+            
+            if(err) {
+                //TODO: code
+            } else {
+                //TODO: code
+            }
+        }
+	}//END while
+    
+    
 	ifs.close();//close the filestream
 
 	return true;
 }
 
+//----helper functions----//
+static bool getLineValue(std::string str, glm::vec3 *vec, const std::string &prefix) {
+    std::istringstream iss(str);
+    std::string word;
+    while(iss >> word) {
+        if(word == prefix || word == (prefix + ":")) {//FIXME: this doesn't check if the word already has the ":" and ths could accept words of type "word::" (same for other getLineValue() methods
+            iss >> word;
+            vec->x = std::stoi(word);
+            iss >> word;
+            vec->x = std::stoi(word);
+            iss >> word;
+            vec->x = std::stoi(word);
+            return 1;
+        } else {
+            std::cout << "prefix not recognised: " << word << std::endl;
+            return 0;
+        }
+    }
+    return 0;
+}
+
+static bool getLineValue(std::string str ,int *val, const std::string &prefix) {
+    std::istringstream iss(str);
+    std::string word;
+    while(iss >> word) {
+        if(word == prefix || word == (prefix + ":")) {
+            iss >> word;
+            *val = std::stoi(word);
+            return 1;
+        } else {
+            std::cout << "prefix not recognised: " << word << std::endl;
+            return 0;
+        }
+    }
+    return 0;
+
+}
+
+static bool getLineValue(std::string str ,float *val, const std::string &prefix) {
+    std::istringstream iss(str);
+    std::string word;
+    while(iss >> word || word == (prefix + ":")) {
+        if(word == prefix) {
+            iss >> word;
+            *val = std::stof(word);
+            return 1;
+        } else {
+            std::cout << "prefix not recognised: " << word << std::endl;
+            return 0;
+        }
+    }
+    return 0;
+}
 
 //----Constructors for helper classes----//
 SceneObject::SceneObject() {
@@ -92,6 +266,16 @@ Mesh::Mesh(const char * path, const glm::vec3 &ambientColor,
 	this->diffuseColor = diffuseColor;
 	this->specularColor = specularColor; 
 	this->shininess = shininess;
+}
+
+Triangle::Triangle(const glm::vec3 &v1, const glm::vec3 &v2, const glm::vec3 &v3, const glm::vec3 &ambientColor, const glm::vec3 &diffuseColor, const glm::vec3 &specularColor, float shininess) {
+    this->v1 = v1;
+    this->v2 = v2;
+    this->v3 = v3;
+    this->ambientColor = ambientColor;
+    this->diffuseColor = diffuseColor;
+    this->specularColor = specularColor;
+    this->shininess = shininess;
 }
 
 Light::Light(const glm::vec3 &posisiton, const glm::vec3 &ambient, const glm::vec3 &diffuse, const glm::vec3 &specular) {
--- Comp371 Final Project MacOS/SceneLoader.h
+++ Comp371 Final Project MacOS/SceneLoader.h
@@ -8,7 +8,102 @@
 #include <fstream>//basic file operations
 #include "glm.hpp"//glm
 #include <string>
+#include <vector>
+#include <sstream>
 
+const bool VERBOSE = true;//MARK: debug verbose on/off
+
+//----Helper objects----//
+class SceneObject {
+public:
+    SceneObject();
+};
+
+//camera object
+class Camera : public SceneObject {
+public:
+    glm::vec3 position;
+    int fov;
+    int focalLength;
+    float aspectRatio;
+    Camera(glm::vec3 position, int fov, int focalLength, float aspectRatio);
+};
+
+//plane object
+class Plane : public SceneObject {
+public:
+    glm::vec3 normal;
+    glm::vec3 position;
+    glm::vec3 ambientColor;
+    glm::vec3 diffuseColor;
+    glm::vec3 specularColor;
+    float shininess;
+    Plane(const glm::vec3 &normal,
+          const glm::vec3 &position,
+          const glm::vec3 &ambientColor,
+          const glm::vec3 &diffuseColor,
+          const glm::vec3 &specularColor,
+          float shininess);
+};
+
+//sphere object
+class Sphere : public SceneObject {
+public:
+    glm::vec3 position;
+    float radius;
+    glm::vec3 ambientColor;
+    glm::vec3 diffuseColor;
+    glm::vec3 specularColor;
+    float shininess;
+    Sphere(const glm::vec3 &position,
+           float radius,
+           const glm::vec3 &ambientColor,
+           const glm::vec3 &diffuseColor,
+           const glm::vec3 &specularColor,
+           float shininess);
+};
+
+//Mesh object
+class Mesh : public SceneObject {
+public:
+    const char * path;
+    glm::vec3 ambientColor;
+    glm::vec3 diffuseColor;
+    glm::vec3 specularColor;
+    float shininess;
+    Mesh(const char * path, const glm::vec3 &ambientColor, const glm::vec3 &diffuseColor, const glm::vec3 &specularColor, float shininess);
+};
+
+class Triangle : public SceneObject {
+public:
+    glm::vec3 v1;
+    glm::vec3 v2;
+    glm::vec3 v3;
+    glm::vec3 ambientColor;
+    glm::vec3 diffuseColor;
+    glm::vec3 specularColor;
+    float shininess;
+    Triangle(const glm::vec3 &v1, const glm::vec3 &v2, const glm::vec3 &v3, const glm::vec3 &ambientColor, const glm::vec3 &diffuseColor, const glm::vec3 &specularColor, float shininess);
+};
+
+//Light Object
+class Light : public SceneObject {
+public:
+    glm::vec3 position;
+    glm::vec3 ambient;
+    glm::vec3 diffuse;
+    glm::vec3 specular;
+    Light(const glm::vec3 &posisiton, const glm::vec3 &ambient, const glm::vec3 &diffuse, const glm::vec3 &specular);
+    
+};
+//getLineValue checks the line prefix for validity and then sets the given variable with the line's value
+//multiple overloads for different variables types
+//TODO: get these to throw exeptions
+static bool getLineValue(std::string str ,glm::vec3 *vec, const std::string &prefix);
+static bool getLineValue(std::string str ,int *val, const std::string &prefix);
+static bool getLineValue(std::string str ,float *val, const std::string &prefix);
+
+//----SceneObj----//
 //Scene objects load data from scene files and 
 //store the data in various objects that are declared bellow
 class SceneObj 
@@ -16,85 +111,17 @@ class SceneObj
 public:
 	SceneObj();
 	SceneObj(const char * path);
+    ~SceneObj();
 	bool loadScene();
 	void setNumberOfObjects(int number);
 	int getNumberOfObjects();
+    //std::vector<SceneObject*> objectArray;//MARK: removable old
+    std::vector<Camera> cameraArray;
+    std::vector<Plane> planeArray;
+    std::vector<Sphere> sphereArray;
+    std::vector<Mesh> meshArray;
+    std::vector<Light> lightArray;
 private:
 	const char * path;
 	int numberOfObjects;
-};
-
-class SceneObject {
-public:
-	SceneObject();
-};
-
-//camera object
-class Camera : public SceneObject {
-public:
-	glm::vec3 position;
-	int fov;
-	int focalLength;
-	float aspectRatio;
-	Camera(glm::vec3 position, int fov, int focalLength, float aspectRatio);
-};
-
-//plane object
-class Plane : public SceneObject {
-public:
-	glm::vec3 normal;
-	glm::vec3 position;
-	glm::vec3 ambientColor;
-	glm::vec3 diffuseColor;
-	glm::vec3 specularColor;
-	float shininess;
-	Plane(const glm::vec3 &normal, 
-		const glm::vec3 &position, 
-		const glm::vec3 &ambientColor, 
-		const glm::vec3 &diffuseColor, 
-		const glm::vec3 &specularColor, 
-		float shininess);
-};
-
-//sphere object
-class Sphere : public SceneObject {
-public:
-	glm::vec3 position;
-	float radius;
-	glm::vec3 ambientColor;
-	glm::vec3 diffuseColor;
-	glm::vec3 specularColor;
-	float shininess;
-	Sphere(const glm::vec3 &position,
-		float radius,
-		const glm::vec3 &ambientColor,
-		const glm::vec3 &diffuseColor,
-		const glm::vec3 &specularColor,
-		float shininess);
-};
-
-//Mesh object
-class Mesh : public SceneObject {
-public:
-	const char * path;
-	glm::vec3 ambientColor;
-	glm::vec3 diffuseColor;
-	glm::vec3 specularColor;
-	float shininess;
-	Mesh(const char * path,
-		const glm::vec3 &ambientColor,
-		const glm::vec3 &diffuseColor,
-		const glm::vec3 &specularColor,
-		float shininess);
-};
-
-//Light Object
-class Light : public SceneObject {
-public:
-	glm::vec3 position;
-	glm::vec3 ambient;
-    glm::vec3 diffuse;
-    glm::vec3 specular;
-    Light(const glm::vec3 &posisiton, const glm::vec3 &ambient, const glm::vec3 &diffuse, const glm::vec3 &specular);
-
 };
